13575a05e4f6f879725f5d983e1e437cfb31e353
hesel <hesel@pop-os.localdomain>
2023-05-25 01:40:31 +0330

    Applying corrections to the main.py file

--- main.py
+++ main.py
@@ -1,4 +1,39 @@
 from users import User
+
+
+def receive_information(key):
+
+    """
+    This function receive users information
+    including username password and phone number
+    when the key value is "1" and return them
+    """
+    if key == 1:
+        username = input("username: ")
+        password = input("password: ")
+        phone_number = input("phone number(optional): ")
+ 
+        if phone_number == "":
+            phone_number = None
+        return  username, password, phone_number 
+    elif key == 2:
+        username = input("username: ")
+        password = input("password: ")
+        return  username, password
+    elif key == 2-2:
+        new_username = input("new username: ")   
+        new_phone_number = input("new phone number: ") 
+        if new_phone_number == "":
+            new_phone_number = None
+        return  new_username, new_phone_number
+    elif  key == 2-3:
+        old_password = input("old_password: ")
+        new_password = input("new password: ") 
+        repeat_new_password = input("repeat new password: ") 
+        return  old_password, new_password, repeat_new_password 
+
+
+
 while True:
     key = input("According to the requested operation,"
     " enter one of the following options:\n"
@@ -14,12 +49,12 @@ while True:
         break
     elif key == "1":
 
-        username, password, phone_number = User.getting_information()  
-        User.do_register(username, password, phone_number)
-        
+        username, password, phone_number = receive_information(1)
+        User.sign_up(username, password, phone_number)
+
     elif key == "2":  
-        username, password = User.getting_information_for_entering_to_user_account()
-        if User.check_validation_of_username_and_password(username, password):
+        username, password = receive_information(2)
+        if User.sign_in(username, password):
             while True:
                 key = input("According to the requested operation,"
                 " enter one of the following options:\n"
@@ -35,26 +70,23 @@ while True:
                 "key:")
             
                 if key == "1":
-                    User.show_registeried_users(username)
+                    User.show_user_informations(username)
                 if key == "2":
-                    new_username, new_phone_number = User.getting_new_information_for_changing_user_account()
-                    User.username_phone_number_editator(username, new_username, new_phone_number)
+                    new_username, new_phone_number = receive_information(2-2)
+                    User.change_username_and_phone_number(username, new_username, new_phone_number)
+                    username = new_username
                 if key == "3":
-                    old_password, new_password, repeat_new_password = User.getting_information_for_entering_to_new_password_account()  
-                    User.check_old_new_repeat_passwords_length_new_password(new_password)
-                    User.check_old_new_repeat_passwords_length_old_password(old_password)
-                    User.check_old_new_repeat_passwords_length_repeat_new_password(repeat_new_password)
-                    User.check_password_is_correct(old_password, username)
-                    User.valid_new_password_and_repeat_new_password(new_password, repeat_new_password)
-                    User.update_password(username, new_password) 
+                    old_password, new_password, repeat_new_password = receive_information(2-3)
+                    if User.valid_new_password(new_password, repeat_new_password):
+                        User.update_password(old_password, username, new_password)
+                      
+                    else:
+                        continue    
+
                 if key == "4":  
                     break 
+        else:
+            continue    
 
-
-
-
-
-            
-        # print(username, password,phone_number)    
 
 
